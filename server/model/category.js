63cb6f0f8cce9d111fcbe216c6657d7344ca9fac
liuziyang <ziyang.liu@roadshare.com>
2017-11-13 18:17:09 +0800

    添加分类标签接口

--- server/model/category.js
+++ server/model/category.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 const CategorSchema = new Schema({
   name: String,
   type: String,
+  description: String,
+  date: Date
 })
 
 module.exports = mongoose.model('category', CategorSchema);
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,5 +1,165 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
-const Category = require('../model/category');
+const CategoryModel = require('../model/category');
 
+router.get('/', (req, res, next) => {
+  const type = req.query.type;
+  let search = {};
+  if (type) {
+    search.type = type;
+  } else {
+    search = {}
+  }
+
+  CategoryModel
+  .find(search)
+  .then((data) => {
+    let tempData = {};
+    data.forEach(item => {
+      if (tempData[item.type]) {
+        tempData[item.type].push(item);
+      } else {
+        tempData[item.type] = [];
+        tempData[item.type].push(item);
+      }
+    })
+    res.status(200);
+    res.json({
+      code: 0,
+      data: tempData
+    })
+  })
+  .catch((e) => {
+    throw new Error(e);
+  })
+})
+
+router.post('/', (req, res, next) => {
+  const category = req.body;
+  category.date = new Date().getTime();
+  CategoryModel.findOne({name: category.name})
+  .then((data) => {
+    if (data) {
+      res.status(400);
+      res.json({
+        code: 1,
+        verror: {
+          msg: '名称重复' 
+        }
+      })
+      return;
+    }
+    const newCategory = new CategoryModel(category);
+    newCategory
+    .save()
+    .then(() => {
+      res.status(200);
+      res.json({
+        code: 0,
+        data:{
+          msg: 'success'
+        }
+      })
+    })
+    .catch(e => {
+      throw new Error(e);
+      res.status(500);
+      res.json({
+        code: 5,
+        verror: {
+          msg: 'something wrong'
+        }
+      })
+    })
+  })
+})
+
+router.put('/:id', (req, res, next) => {
+  const categoryData = req.body;
+  const categoryId = req.params.id;
+  CategoryModel
+  .find({'_id': categoryId})
+  .then((category) => {
+    if (!category.length) {
+      res.status(400);
+      res.json({
+        code: 4,
+        verror: {
+          msg: '没有相关记录'
+        }
+      })
+      return;
+    }
+    console.log(categoryData)
+    CategoryModel
+    .update({'_id': categoryId}, {$set: categoryData})
+    .then(() => {
+      res.status(200);
+      res.json({
+        code: 0,
+        data: {
+          msg: 'success'
+        }
+      })
+    })
+    .catch(e => {
+      throw new Error(e);
+      res.status(500);
+      res.json({
+        code: 5,
+        verror: 'Something error'
+      })
+    })
+  }).catch(e => {
+    throw new Error(e);
+    res.status(500);
+    res.json({
+      code: 5,
+      verror: {
+        msg: 'Something error'
+      }
+    })
+  })
+})
+
+router.delete('/:id', (req, res, next) => {
+  const categoryId = req.params.id;
+  CategoryModel
+  .find({'_id': categoryId})
+  .then(() => {
+    CategoryModel
+    .remove({'_id': categoryId})
+    .then(() => {
+      res.status(200);
+      res.json({
+        code: 0,
+        data: {
+          msg: 'success'
+        }
+      })
+    })
+    .catch(e => {
+      throw new Error(e);
+      rs.status(500);
+      res.json({
+        code: 5,
+        verror: {
+          msg: 'Something error'
+        }
+      })
+    })
+  })
+  .catch(e => {
+    throw new Error(e);
+    res.status(500);
+    res.json({
+      code: 5,
+      verror: {
+        msg: 'Something error'
+      }
+    })
+  })
+})
+
+module.exports = router;
