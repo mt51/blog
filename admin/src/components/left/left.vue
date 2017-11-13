4618f1fa984628573e3205584fcbbe58abddb95e
liuziyang <798230885@qq.com>
2017-11-14 00:13:36 +0800

    调整侧边栏

--- admin/src/components/left/left.vue
+++ admin/src/components/left/left.vue
@@ -1,74 +1,65 @@
 <template>
   <div class="nav">
-    <Menu theme="dark" width="auto" active-name="home">
+    <Menu theme="dark" width="auto" :active-name="currentPath">
       <div class="layout-logo-left"></div>
-      <Submenu name="home" class="empty">
-        <template slot="title">
-          <router-link to="home"><Icon type="home"></Icon>首页</router-link>
-        </template>
-      </Submenu>
-      <Submenu name="1">
+      <router-link to="home">
+        <MenuItem name="home">
+          <Icon type="home"></Icon>
+          <span class="layout-text">首页</span>
+        </MenuItem>
+      </router-link>
+      <Submenu name="article">
         <template slot="title">
           <Icon type="document-text"></Icon>文章管理
         </template>
-        <MenuItem name="article"><router-link to="article">文章列表</router-link></MenuItem>
-        <MenuItem name="publish"><router-link to="publish">文章添加</router-link></MenuItem>
+        <router-link to="article"><MenuItem name="article">文章列表</MenuItem></router-link>
+        <router-link to="publish"><MenuItem name="publish">文章添加</MenuItem></router-link>
       </Submenu>
-      <Submenu name="1">
+      <Submenu name="pages">
         <template slot="title">
           <Icon type="paper-airplane"></Icon>页面管理
         </template>
-        <MenuItem name="页面列表"><router-link to="pages">页面列表</router-link></MenuItem>
-        <MenuItem name="addpage"><router-link to="addpage">添加页面</router-link></MenuItem>
+        <router-link to="pages"><MenuItem name="页面列表">页面列表</MenuItem></router-link>
+        <router-link to="addpage"><MenuItem name="addpage">添加页面</MenuItem></router-link>
       </Submenu>
-      <Submenu name="1">
+      <Submenu name="themes">
         <template slot="title">
           <Icon type="tshirt"></Icon>主题管理
         </template>
-        <MenuItem name="themes"><router-link to="themes">主题列表</router-link></MenuItem>
-        <MenuItem name="editheme"><router-link to="editheme">编辑主题</router-link></MenuItem>
+        <router-link to="themes"><MenuItem name="themes">主题列表</MenuItem></router-link>
+        <router-link to="editheme"><MenuItem name="editheme">编辑主题</MenuItem></router-link>
       </Submenu>
-      <!-- <Submenu name="4">
-        <template slot="title">
-          <Icon type="star"></Icon>分类管理
-        </template>
-        <MenuItem name="categorys"><router-link to="categorys">分类列表</router-link></MenuItem>
-        <MenuItem name="addcategory"><router-link to="addcategory">添加分类></router-link></MenuItem>
-      </Submenu> -->
-      <Submenu name="category" class="empty">
-        <template slot="title">
-          <router-link to="categorys"><Icon type="pricetag"></Icon>分类管理</router-link>
-        </template>
-      </Submenu>
-      <Submenu name="tags" class="empty">
-        <template slot="title">
-          <router-link to="tags"><Icon type="star"></Icon>标签管理</router-link>
-        </template>
-      </Submenu>
-      <!-- <Submenu name="4">
-        <template slot="title">
-          <Icon type="pricetag"></Icon>标签管理
-        </template>
-        <MenuItem name="tags"><router-link to="tags">标签列表</router-link></MenuItem>
-        <MenuItem name="addtag"><router-link to="addtag">添加标签</router-link></MenuItem>
-      </Submenu> -->
-      <Submenu name="user" class="empty">
-        <template slot="title">
-          <router-link to="user"><Icon type="settings"></Icon>用户设置</router-link>
-        </template>
-      </Submenu>
+      <router-link to="categorys">
+        <MenuItem name="categorys">
+          <Icon type="pricetag"></Icon>
+          <span class="layout-text">分类管理</span>
+        </MenuItem>
+      </router-link>
+      <router-link to="tags">
+        <MenuItem name="tags">
+          <Icon type="star"></Icon>
+          <span class="layout-text">标签管理</span>
+        </MenuItem>
+      </router-link>
+      <router-link to="user">
+        <MenuItem name="user">
+          <Icon type="settings"></Icon>
+          <span class="layout-text">用户设置</span>
+        </MenuItem>
+      </router-link>
       <Submenu name="">
         <template slot="title">
           <Icon type="gear-b"></Icon>系统设置
         </template>
-        <MenuItem name="base"><router-link to="base">基本设置</router-link></MenuItem>
-        <MenuItem name="statistics"><router-link to="statistics">统计代码</router-link></MenuItem>
+        <router-link to="base"><MenuItem name="base">基本设置</MenuItem></router-link>
+        <router-link to="statistics"><MenuItem name="statistics">统计代码</MenuItem></router-link>
       </Submenu>
-      <Submenu name="draft" class="empty">
-        <template slot="title">
-          <router-link to="draft"><Icon type="ios-compose"></Icon>草稿箱</router-link>
-        </template>
-      </Submenu>
+      <router-link to="draft">
+        <MenuItem name="draft">
+          <Icon type="ios-compose"></Icon>
+          <span class="layout-text">草稿箱</span>
+        </MenuItem>
+      </router-link>
     </Menu>
   </div>
 </template>
@@ -77,7 +68,11 @@
     name: 'leftNav',
     data () {
       return {
+        currentPath: ''
       }
+    },
+    mounted () {
+      this.currentPath = this.$route.name
     }
   }
 </script>
@@ -137,6 +132,13 @@
         }
       }
     }
+    .ivu-menu-item {
+      a {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+      }
+    }
     .ivu-menu-item-selected{
       a {
         color: #fff;
@@ -149,6 +151,14 @@
       color: rgba(255,255,255,.7);
       &:hover{
         color: #fff;
+      }
+    }
+    .ivu-menu-item-active{
+      color: #2d8cf0;
+      border-right: 2px solid #2d8cf0;
+      background: #363e4f;
+      &:hover{
+        background: #363e4f;
       }
     }
   }
