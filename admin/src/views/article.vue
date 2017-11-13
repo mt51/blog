032aa1c95432d9b289e22b2815a09efd61b5e805
liuziyang <ziyang.liu@roadshare.com>
2017-11-13 18:15:54 +0800

    完善文章管理

--- admin/src/views/article.vue
+++ admin/src/views/article.vue
@@ -1,3 +1,130 @@
 <template>
-  <div>article</div>
+  <div class="article">
+    <Table :loading="loading" :columns="columns" :data="data" stripe></Table>
+    <Page :total="total" on-change="fetchArticleList" class-name="page"></Page>
+  </div>
 </template>
+<script>
+  import formatDate from '@/util/date'
+  export default {
+    name: 'article',
+    data () {
+      return {
+        columns: [
+          {
+            title: '标题',
+            key: 'title',
+            ellipsis: true
+          }, {
+            title: '作者',
+            key: 'author'
+          }, {
+            title: '日期',
+            key: 'date'
+          }, {
+            title: '摘要',
+            key: 'description',
+            ellipsis: true
+          }, {
+            title: '操作',
+            key: 'action',
+            render: (h, p) => {
+              return h('div', {
+                class: {
+                  btnGroup: true
+                }
+              }, [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.edit(p)
+                    }
+                  }
+                }, '编辑'),
+                h('Button', {
+                  props: {
+                    type: 'ghost',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.deleteTips(p)
+                    }
+                  }
+                }, '删除')
+              ])
+            }
+          }
+        ],
+        data: [],
+        loading: false,
+        total: 0,
+        currentArticleId: '',
+        tags: [],
+        category: []
+      }
+    },
+    created () {
+      this.fetchArticleList(1)
+      this.fetchTagsAndCategorys()
+    },
+    methods: {
+      fetchArticleList (page) {
+        this.axios.get('/api/article?page=' + page)
+          .then((response) => {
+            if (response.data.code === 0) {
+              this.data = response.data.data
+              this.total = response.data.total
+              this.data.forEach(item => {
+                item.date = formatDate(item.date, 'yyyy-MM-dd')
+              })
+            }
+          })
+      },
+      edit (p) {
+        console.log(p)
+        this.router.push({name: 'edit', params: {id: p.row['_id']}})
+      },
+      deleteArticle () {
+        this.axios.delete('/api/article/' + this.currentArticleId)
+        .then(response => {
+          if (response.data.code === 0) {
+            this.$Message.success('删除成功')
+          } else {
+            this.$Message.error('删除失败')
+          }
+        })
+      },
+      deleteTips (p) {
+        this.currentArticleId = p.row['_id']
+        this.$Modal.confirm({
+          title: '提醒',
+          content: '确认删除？',
+          okText: '删除',
+          cancelText: '取消',
+          onOk: this.deleteArticle
+        })
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .article {
+    margin-left: 20px;
+    margin-right: 20px;
+    .page{
+      margin-top: 20px;
+      text-align: right;
+    }
+  }
+</style>
--- admin/src/views/publish.vue
+++ admin/src/views/publish.vue
@@ -4,15 +4,15 @@
       <Input v-model="articleData.title" placeholder="标题" style="width: 300px"></Input>
       <Input v-model="articleData.description" placeholder="简介" style="width: 300px"></Input>
       <Select v-model="articleData.tag" style="width:200px" placeholder="标签">
-        <Option v-for="item in tags" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Option v-for="(item, key) in tags" :value="item.name" :key="key">{{ item.name }}</Option>
       </Select>
       <Select v-model="articleData.category" style="width:200px" placeholder="分类">
-        <Option v-for="item in categorys" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Option v-for="(item, key)in categorys" :value="item.name" :key="key">{{ item.name }}</Option>
       </Select>
       <Button type="dashed">存为草稿</Button>
       <Button type="primary" @click.native="publish">发布</Button>
     </div>
-    <mavonEditor :ishljs="true" v-model="articleData.mdcont" @imgAdd="handleAddImg"/>
+    <mavonEditor v-model="articleData.mdcont" @imgAdd="handleAddImg"/>
   </div>
 </template>
 <script>
@@ -33,6 +33,9 @@
         categorys: []
       }
     },
+    created () {
+      this.fetchTagsAndCategorys()
+    },
     methods: {
       publish () {
         this.axios.post('/api/article', this.articleData).then(response => {
@@ -43,8 +46,14 @@
           }
         })
       },
+      fetchTagsAndCategorys () {
+        this.axios.get('/api/category?type')
+        .then(response => {
+          this.tags = response.data.data.tag
+          this.categorys = response.data.category
+        })
+      },
       handleAddImg () {
-        
       }
     },
     components: {
@@ -52,9 +61,8 @@
     }
   }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
   .publish{
-    
     padding: 0 20px;
     .markdown-body{
       height: calc(100vh - 200px);
