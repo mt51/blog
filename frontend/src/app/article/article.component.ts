03e9125233259cf2816d7191149d156e97c2ea3f
liuziyang <ziyang.liu@roadshare.com>
2017-11-24 23:04:05 +0800

    完善文章详情页组件

--- frontend/src/app/article/article.component.ts
+++ frontend/src/app/article/article.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ArticleService } from './article.service'
 
 @Component({
   selector: 'app-article',
+  providers: [ArticleService],
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  articleInfo: object = {}
+  constructor(
+    private article: ArticleService,
+    private router: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const articleId = this.router.snapshot.params.id
+    this.article.fetchArticleDetail(articleId)
+      .then(response => {
+        if (response.body.code === 0) {
+          this.articleInfo = response.body.data
+        }
+      })
   }
 
 }
