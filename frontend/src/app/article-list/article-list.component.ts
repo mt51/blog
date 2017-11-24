18fad9c7eb4fa3889669c7e62b1638fb3fb18e1d
liuziyang <ziyang.liu@roadshare.com>
2017-11-24 23:03:05 +0800

    完善文章列表组件

--- frontend/src/app/article-list/article-list.component.ts
+++ frontend/src/app/article-list/article-list.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ArticlesListService } from './article-list.service';
+
 
 @Component({
   selector: 'app-article-list',
+  providers: [ArticlesListService],
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private article: ArticlesListService,
+    private router: ActivatedRoute
+  ) { }
 
-  artilceList: object[] = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}]
+  artilceList: object[] = []
 
   ngOnInit() {
+    const page = this.router.snapshot.params.page || 1
+    this.article.fetchArtlclesList({page: page})
+      .then(response => {
+        if (response.body.code === 0) {
+          this.artilceList = response.body.data
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
 }
