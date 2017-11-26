57e9b09ee0667e97b76769b64262d6cb9f5fd267
liuziyang <798230885@qq.com>
2017-11-26 23:23:08 +0800

    更新路由

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,15 +6,18 @@ import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleComponent } from './article/article.component';
 import { AboutComponent } from './about/about.component';
 import { LinkComponent } from './link/link.component';
+import { CategoriesComponent} from './categories/categories.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/index', pathMatch: 'full'},
   {path: 'index', component: IndexComponent},
   {path: 'articles/pages/:page', component: ArticleListComponent},
+  {path: 'categories/:category', component: CategoriesComponent},
   {path: 'article/:id', component: ArticleComponent},
   {path: 'about', component: AboutComponent},
   {path: 'links', component: LinkComponent},
-  {path: '**', component: IndexComponent},
+  {path: '**', component: NotFoundComponent},
 ]
 
 @NgModule({
