53234cbe7047c7b4242526f4f12b62b4109f5e1c
liuziyang <798230885@qq.com>
2017-12-01 00:34:17 +0800

    修改项目构建结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 
-gulp.task('default', ['admin', 'front']);
+gulp.task('default', ['admin', 'front', 'static']);
 
 gulp.task('admin', function(){
-  return gulp.src('admin/dist/**')
+  return gulp.src('admin/dist/index.html')
     .pipe(gulp.dest('server/views/admin'))
+})
+
+gulp.task('static', function(){
+  return gulp.src('admin/dist/static/**')
+    .pipe(gulp.dest('server/public/static'))
 })
 
 gulp.task('front', function(){
