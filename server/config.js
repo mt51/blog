b187dd4b2bc939ff5ed82b2188ab25ea4a6bbd33
liuziyang <798230885@qq.com>
2017-12-01 00:36:00 +0800

    更新配置

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   databaseUrl:'mongodb://localhost/blog',
-  whitelist: ['http://localhost:8082','http://localhost:8083','http://localhost:8080'],
+  whitelist: [],
   secret: 'liuziyang!@#$%^',
   AK: 'AA1KlvswTPh0cFrvOaNchiSRZi_Vj9Le3OpxWLY_',
   SK: 'PvVGx81PuWizLTJfAC0KU9X4ozsBb7OJ5VWvBcAU'
