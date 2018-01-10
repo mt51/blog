12e3429e28ec11310a84f172609f4f69d04c0b1a
liuziyang <ziyang.liu@roadshare.com>
2018-01-10 10:06:17 +0800

    Revert "config"
    This reverts commit e0e33221840dd8552013e0a68930b746478d6d4a.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,12 @@
 import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { platformBrowser }    from '@angular/platform-browser'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModuleNgFactory } from './app/app.module.ngfactory';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
