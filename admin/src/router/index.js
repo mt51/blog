54d7fb917d572868658c8efae41b45f113e6e296
liuziyang <ziyang.liu@roadshare.com>
2018-01-11 17:42:29 +0800

    admin  组件使用异步模式

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,21 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Signin from '@/views/signin'
-import Home from '@/views/home'
-import Category from '@/views/categorys'
-import Article from '@/views/article'
-import Base from '@/views/base'
-import Draft from '@/views/draft'
-import EditTheme from '@/views/edittheme'
-import Pages from '@/views/pages'
-import Publish from '@/views/publish'
-import Statistics from '@/views/statistics'
-import Tags from '@/views/tags'
-import Themes from '@/views/themes'
-import User from '@/views/user'
-import Dashboard from '@/views/dashboard'
-import Import from '@/views/import'
-import Link from '@/views/links'
+// import Signin from '@/views/signin'
+// import Home from '@/views/home'
+// import Category from '@/views/categorys'
+// import Article from '@/views/article'
+// import Base from '@/views/base'
+// import Draft from '@/views/draft'
+// import EditTheme from '@/views/edittheme'
+// import Pages from '@/views/pages'
+// import Publish from '@/views/publish'
+// import Statistics from '@/views/statistics'
+// import Tags from '@/views/tags'
+// import Themes from '@/views/themes'
+// import User from '@/views/user'
+// import Dashboard from '@/views/dashboard'
+// import Import from '@/views/import'
+// import Link from '@/views/links'
+
+const Signin = () => import('@/views/signin')
+const Home = () => import('@/views/home')
+const Category = () => import('@/views/categorys')
+const Article = () => import('@/views/article')
+const Base = () => import('@/views/base')
+const Draft = () => import('@/views/draft')
+const EditTheme = () => import('@/views/edittheme')
+const Pages = () => import('@/views/pages')
+const Publish = () => import('@/views/publish')
+const Statistics = () => import('@/views/statistics')
+const Tags = () => import('@/views/tags')
+const Themes = () => import('@/views/themes')
+const User = () => import('@/views/user')
+const Dashboard = () => import('@/views/dashboard')
+const Import = () => import('@/views/import')
+const Link = () => import('@/views/links')
 
 Vue.use(Router)
 
