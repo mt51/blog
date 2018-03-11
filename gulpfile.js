eb287f292c1744098e1bd8841af0485cff08ebaa
liuziyang <798230885@qq.com>
2018-03-11 23:01:52 +0800

    更新打包配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 
-gulp.task('default', ['clean'], function(){
+gulp.task('default', ['clean'], function() {
   gulp.start('admin', 'front', 'static')
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
   return gulp.src(['server/views/front', 'server/public/static', 'server/views/admin', 'frontend/dist/favicon.ico'])
-  .pipe(clean())
+    .pipe(clean())
 })
 
-gulp.task('admin', function(){
+gulp.task('admin', function() {
   return gulp.src('admin/dist/index.html')
     .pipe(gulp.dest('server/views/admin'))
 })
 
-gulp.task('static', function(){
+gulp.task('static', function() {
   return gulp.src('admin/dist/static/**')
     .pipe(gulp.dest('server/public/static'))
 })
 
-gulp.task('front', function(){
-  return gulp.src('frontend/dist/**')
+gulp.task('front', function() {
+  return gulp.src('react-blog/build/**')
     .pipe(gulp.dest('server/views/front'))
 })
