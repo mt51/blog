7fec00198b04cc3eef320f55fe3bdd3c5ec0035a
liuziyang <798230885@qq.com>
2018-04-10 23:45:46 +0800

    添加错误控制和token验证

--- admin2.0/app.js
+++ admin2.0/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const config = require('config')
+const expressJwt = require('express-jwt')
 const resolve = file => path.resolve(__dirname, file)
 
 const isProd = process.env.NODE_ENV === 'production'
@@ -21,11 +23,29 @@ app.use(express.json());
 app.use('/dist', serve('./dist', true))
 app.use('/public', serve('./public', true))
 
+// app.use(expressJwt({ secret: config.get('jwt.secret')}).unless(req => {
+//   return req.method === 'POST'
+// }))
+
 app.use('/api', routes)
 
 app.use(viewMiddleware(app))
 
-const port = process.env.PORT || 8080
+
+// error handler
+app.use(function(err, req, res, next) {
+  if (err.status === 401) {
+    res.status(401)
+    return res.json({errMsg: '身份验证失败，请重新登录'})
+  }
+  console.log(err)
+  res.status(err.status || 500);
+  console.log('状态码:', err.status)
+  console.log('错误信息:',err.message)
+  console.log(err.stack)
+});
+
+const port = config.get('port')
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
